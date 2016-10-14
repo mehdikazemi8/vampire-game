b10d0498d856ba7ef6006cf7accef4fb3f13afe8
Mehdi Kazemi <mehdi.kazemi8@gmail.com>
2016-10-14 08:44:50 +0330

    fragments created

---
+++ app/src/main/java/ir/ugstudio/vampire/views/activities/main/fragments/NotificationsFragment.java
@@ -0,0 +1,23 @@
+package ir.ugstudio.vampire.views.activities.main.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ir.ugstudio.vampire.R;
+
+public class NotificationsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_notifications, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
