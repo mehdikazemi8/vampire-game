168eacb2e27c5de2cddf330e8ba34bff3e4f78ef
Mehdi Kazemi <mehdi.kazemi8@gmail.com>
2016-12-07 18:54:48 +0330

    prettify TowerWallFragment

--- app/src/main/java/ir/ugstudio/vampire/views/activities/main/adapters/MessageViewAdapter.java
+++ app/src/main/java/ir/ugstudio/vampire/views/activities/main/adapters/MessageViewAdapter.java
@@ -1,18 +1,23 @@
 package ir.ugstudio.vampire.views.activities.main.adapters;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.squareup.picasso.Picasso;
+
 import java.util.List;
 
 import ir.ugstudio.vampire.R;
+import ir.ugstudio.vampire.managers.AvatarManager;
 import ir.ugstudio.vampire.models.TowerMessage;
 
 public class MessageViewAdapter extends RecyclerView.Adapter<MessageViewHolder> {
 
     private List<TowerMessage> items;
+    private Context context;
 
     public MessageViewAdapter(List<TowerMessage> items) {
         this.items = items;
@@ -23,6 +28,7 @@ public class MessageViewAdapter extends RecyclerView.Adapter<MessageViewHolder> 
         View itemView = LayoutInflater.
                 from(parent.getContext()).
                 inflate(R.layout.template_message, parent, false);
+        context = parent.getContext();
         return new MessageViewHolder(itemView);
     }
 
@@ -30,6 +36,9 @@ public class MessageViewAdapter extends RecyclerView.Adapter<MessageViewHolder> 
     public void onBindViewHolder(MessageViewHolder holder, int position) {
         holder.username.setText(items.get(position).getUsername());
         holder.message.setText(String.valueOf(items.get(position).getMessage()));
+
+        Picasso.with(context).load(AvatarManager.getResourceId(context, items.get(position).getAvatar()))
+                .into(holder.avatar);
     }
 
     @Override
--- app/src/main/java/ir/ugstudio/vampire/views/activities/main/adapters/MessageViewHolder.java
+++ app/src/main/java/ir/ugstudio/vampire/views/activities/main/adapters/MessageViewHolder.java
@@ -2,18 +2,22 @@ package ir.ugstudio.vampire.views.activities.main.adapters;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import ir.ugstudio.vampire.R;
+import ir.ugstudio.vampire.views.custom.CustomTextView;
 
 public class MessageViewHolder extends RecyclerView.ViewHolder {
-    public TextView message;
+    public CustomTextView message;
     public TextView username;
+    public ImageView avatar;
 
     public MessageViewHolder(View view) {
         super(view);
 
         username = (TextView) view.findViewById(R.id.username);
-        message = (TextView) view.findViewById(R.id.message);
+        message = (CustomTextView) view.findViewById(R.id.message);
+        avatar = (ImageView) view.findViewById(R.id.avatar);
     }
 }
