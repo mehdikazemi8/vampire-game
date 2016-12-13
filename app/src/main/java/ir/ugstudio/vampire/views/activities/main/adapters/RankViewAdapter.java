f147823b0359de547e0c33762f801971b13db450
Mehdi Kazemi <mehdi.kazemi8@gmail.com>
2016-12-13 16:46:46 +0330

    update ranklist layout and add avatar to it

--- app/src/main/java/ir/ugstudio/vampire/views/activities/main/adapters/RankViewAdapter.java
+++ app/src/main/java/ir/ugstudio/vampire/views/activities/main/adapters/RankViewAdapter.java
@@ -6,10 +6,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.squareup.picasso.Picasso;
+
 import java.util.List;
 
 import ir.ugstudio.vampire.R;
-import ir.ugstudio.vampire.models.Notification;
+import ir.ugstudio.vampire.managers.AvatarManager;
 import ir.ugstudio.vampire.models.User;
 
 public class RankViewAdapter extends RecyclerView.Adapter<RankViewHolder> {
@@ -27,6 +29,7 @@ public class RankViewAdapter extends RecyclerView.Adapter<RankViewHolder> {
         View itemView = LayoutInflater.
                 from(parent.getContext()).
                 inflate(R.layout.template_rank, parent, false);
+        context = parent.getContext();
         return new RankViewHolder(itemView);
     }
 
@@ -35,6 +38,10 @@ public class RankViewAdapter extends RecyclerView.Adapter<RankViewHolder> {
         holder.username.setText(items.get(position).getUsername());
         holder.rank.setText(String.valueOf(items.get(position).getRank()));
         holder.score.setText(String.valueOf(items.get(position).getScore()));
+
+        Picasso.with(context).load(
+                AvatarManager.getResourceId(context, items.get(position).getAvatar())
+        ).into(holder.avatar);
     }
 
     @Override
--- app/src/main/java/ir/ugstudio/vampire/views/activities/main/adapters/RankViewHolder.java
+++ app/src/main/java/ir/ugstudio/vampire/views/activities/main/adapters/RankViewHolder.java
@@ -2,24 +2,27 @@ package ir.ugstudio.vampire.views.activities.main.adapters;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import ir.ugstudio.vampire.R;
+import ir.ugstudio.vampire.views.custom.CustomTextView;
 
 public class RankViewHolder extends RecyclerView.ViewHolder {
 
     public LinearLayout container;
-    public TextView username;
-    public TextView rank;
-    public TextView score;
+    public CustomTextView username;
+    public CustomTextView rank;
+    public CustomTextView score;
+    public ImageView avatar;
 
     public RankViewHolder(View view) {
         super(view);
 
         container = (LinearLayout) view.findViewById(R.id.container);
-        username = (TextView) view.findViewById(R.id.username);
-        rank = (TextView) view.findViewById(R.id.rank);
-        score = (TextView) view.findViewById(R.id.score);
+        username = (CustomTextView) view.findViewById(R.id.username);
+        rank = (CustomTextView) view.findViewById(R.id.rank);
+        score = (CustomTextView) view.findViewById(R.id.score);
+        avatar = (ImageView) view.findViewById(R.id.avatar);
     }
 }
